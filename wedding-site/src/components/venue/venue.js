2e05159b6a4fba4ebb0a126f5e809be238e1af2e
Mike Walker <michael.walker@softwire.com>
2020-01-18 12:10:02 +0000

    placeholder venue section

---
+++ wedding-site/src/components/venue/venue.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './venue.scss';
+import {Section} from "../section/Section";
+
+export function Venue() {
+    return (
+        <Section className="venue" title="The Palm House">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+                dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
+                Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+            </p>
+
+        </Section>
+    );
+}
--- wedding-site/src/pages/index.js
+++ wedding-site/src/pages/index.js
@@ -3,12 +3,14 @@ import { Hero } from "../components/hero/Hero";
 import "../styles/reset.scss";
 import {Introduction} from "../components/introduction/Introduction";
 import {Footer} from "../components/footer/Footer";
+import {Venue} from "../components/venue/venue";
 
 function IndexPage() {
     return (
         <main>
             <Hero/>
             <Introduction/>
+            <Venue/>
             <Footer/>
         </main>
     );
