af0b49f399bbc3f2af9e2f19e5397488895019a1
Mike Walker <michael.walker@softwire.com>
2020-01-19 21:35:28 +0000

    add banner component

---
+++ wedding-site/src/components/banner/banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './banner.scss';
+import PropTypes from 'prop-types';
+
+function Banner(props) {
+    return (
+        <section className="banner" style={{"background-image": `url("${props.image}")`}}>
+            <div className="titleContainer">
+                <h1>{props.title}</h1>
+            </div>
+        </section>
+    );
+}
+
+Banner.propTypes = {
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+};
+
+export { Banner };
---
+++ wedding-site/src/pages/rsvp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Banner} from "../components/banner/banner";
+
+export default function rsvp() {
+    return (
+        <main>
+            <Banner image="/static/images/banner-image.jpg" title="R.S.V.P"/>
+        </main>
+    );
+}
